e3189c245a8e839da5ee3a249fa8f77f1cb41864
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-03 09:40:15 -0300

    Ejemplo de concatenacion

---
+++ UseVar/src/Concatenacion.java
@@ -0,0 +1,10 @@
+public class Concatenacion {
+    public static void main(String[] args) {
+
+        var cadena1 = "Hola,";
+        var cadena2 = "Buenos dias!!";
+
+        var cadena3 = cadena1 + " " + cadena2;
+        System.out.println(cadena3);
+    }
+}
