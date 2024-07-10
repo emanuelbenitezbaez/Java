5e18799df90a28ce034db81f761de8f6e5ec671b
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-10 17:57:19 -0300

    Comparacion de cadenas con == y metodo de string equals y equalsIgnoreCase

--- SentenciasCondicionales/src/EjercicioCondicional.java
+++ SentenciasCondicionales/src/EjercicioCondicional.java
@@ -16,9 +16,22 @@ public class EjercicioCondicional {
         }else {
             System.out.println("Son distintas(en referencia de memoria).");
         }
+        //PARA COMPARAR DEBEMOS USAR EL METODO EQUALS(METODO DE LA CLASE STRING)
+        //COMPARAMOS EL CONTENIDO Y NO LA REFERENCIA
 
-        
 
 
+        if (cadena1.equals(cadena3)){
+            System.out.println("Son iguales(en valor).");
+        }else {
+            System.out.println("Son distintas(en valor).");
+        }
+        //TENER EN CUENTA QUE JAVA ES CASE SENSITIVE Y NO ES LO MISMO CADENA QUE Cadena.
+
+        if (cadena1.equalsIgnoreCase(cadena3)){
+            System.out.println("Son iguales(en valor).");
+        }else {
+            System.out.println("Son distintas(en valor equalsIgnoreCase).");
+        }
     }
 }
