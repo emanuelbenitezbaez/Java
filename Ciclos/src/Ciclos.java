4013ceec75138146725819bcd3e6503f27d6b2b7
Emanuel <emanuelbenitezbaez@gmail.com>
2024-08-06 15:27:04 -0300

    Ciclos : while

---
+++ Ciclos/src/Ciclos.java
@@ -0,0 +1,14 @@
+public class Ciclos {
+    public static void main(String[] args) {
+        //WHILE
+        //Repite la ejecución del bloque de codigo mientras la condicion sea verdadera.
+
+        var number = 20;
+        var contador = 1;
+        while (contador <= number){
+            System.out.println("Numero:" + contador);
+            contador++;
+        }
+
+    }
+}
