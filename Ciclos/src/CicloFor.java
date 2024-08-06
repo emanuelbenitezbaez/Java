5383d17bfdaf050bc98f290a471163a1c0e377ce
Emanuel <emanuelbenitezbaez@gmail.com>
2024-08-06 15:37:58 -0300

    Ciclos - For

---
+++ Ciclos/src/CicloFor.java
@@ -0,0 +1,19 @@
+public class CicloFor {
+    public static void main(String[] args) {
+        //CICLO FOR
+/*
+El ciclo for es una estructura de control en Java que
+nos permite ejecutar un bloque de código un número determinado de veces.
+
+for (inicialización; condición; incremento) {
+    // Código a ejecutar en cada iteración
+}
+
+ */
+        var valor_maximo = 10;
+
+        for (int i = 1; i <= valor_maximo; i++) {
+            System.out.println("El valor de [i] es: " + i);
+        }
+    }
+}
