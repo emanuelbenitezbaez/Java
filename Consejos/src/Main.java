d82abdd4271de0ed149dcfe8ab86ff0e53f523cb
Emanuel <emanuelbenitezbaez@gmail.com>
2024-10-31 11:23:53 -0300

    Update Main.java

--- Consejos/src/Main.java
+++ Consejos/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         /*
         Declara cinco variables de tipo int y realiza las siguientes operaciones: (a + b) * (c - d) / e y a + (b * (c / (d - e))).
                 Imprime los resultados en la consola.
-*/
+        */
         int a = 1;
         int b = 2;
         int c = 3;
@@ -21,11 +21,11 @@ public class Main {
         System.out.println(resultado1);
         System.out.println(resultado2);
                 // Ejercicio #2.
-/*
+        /*
         Declara dos variables de tipo double que representen la base y la altura de un rectángulo.
                 Calcula el área del rectángulo utilizando la fórmula base * altura y el perímetro utilizando la fórmula 2 * (base + altura).
                 Imprime los resultados en la consola.
- */
+        */
         double base = 10;
         double altura = 2;
         double area_rec = base * altura;
