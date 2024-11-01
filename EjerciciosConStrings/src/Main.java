e6744ef73d8a8461d3d9ee5542bd3181ecea25cd
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-01 10:06:42 -0300

    Ejercicios metodos con strings

--- EjerciciosConStrings/src/Main.java
+++ EjerciciosConStrings/src/Main.java
@@ -11,8 +11,6 @@ Convierte la cadena a minúsculas.
 Verifica si la cadena contiene "Programar".
 Imprime los resultados en la consola.
 
-
-
 Ejercicio #2.
 
 Dado el nombre completo "Juan Pérez Rodríguez", realiza las siguientes tareas:
@@ -20,8 +18,6 @@ Extrae el primer nombre.
 Extrae el primer apellido.
 Obtén el primer carácter del segundo apellido.
 
-
-        *
         *
         */
         String cadena = "Programar en Java es emocionante";
@@ -36,5 +32,15 @@ Obtén el primer carácter del segundo apellido.
 
         boolean contiene = cadena.contains("Programar");
         System.out.println("La cadena contiene la palabra programar? " + contiene);
+
+        String nombre_completo = "Juan Pérez Rodríguez";
+        String nombre_extraido = nombre_completo.substring(0,5);
+        System.out.println("Nombre extraido: " + nombre_extraido);
+
+        String primer_apellido_extraido = nombre_completo.substring(5,11);
+        System.out.println("Nombre extraido: " + primer_apellido_extraido);
+
+        char caracter_segundo_apellido = nombre_completo.charAt(11);
+        System.out.println("Primer caracter del segundo apellido: " + caracter_segundo_apellido);
     }
 }
