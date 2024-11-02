01a57e3a4fd871d03b2cd99b9b84ee819a23a6f6
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-02 09:46:36 -0300

    Ingreso de datos por consola

---
+++ IngresoDatos/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese como esta el clima hoy en su ciudad:");
+        String clima_dia = entrada.nextLine();
+        System.out.println(clima_dia);
+
+    }
+}
