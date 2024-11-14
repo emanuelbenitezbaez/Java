f4aa1effe4c376cf0d8a9a6caf54ed5e039cde0a
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-14 08:56:00 -0300

    Ejercicio Listas

--- EjerciciosLista/src/Main.java
+++ EjerciciosLista/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,6 +8,18 @@ public class Main {
         //
         //Declara una lista de enteros y agrega los valores {5, 10, 15, 20, 25}. Imprime el primer y el último elemento de la lista.
 
-        
+        List<Integer> enteros = new ArrayList<>();
+        enteros.add(5);
+        enteros.add(10);
+        enteros.add(15);
+        enteros.add(20);
+        enteros.add(25);
+
+        System.out.println("Primer elemento de la lista: " + enteros.getFirst());
+        System.out.println("Ultimo elemento de la lista: " + enteros.getLast());
+
+        for (int i = 0; i<1;i++){
+            System.out.println("Elementos de la lista : " + enteros);
+        }
     }
 }
