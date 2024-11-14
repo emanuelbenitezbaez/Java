ff3d0fb012ffb3c39d24a5dbfcacd00ca3abbbbc
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-14 08:38:54 -0300

    Ejercicio list

---
+++ EjerciciosLista/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Ejercicio #2.
+        //
+        //Declara una lista de enteros y agrega los valores {5, 10, 15, 20, 25}. Imprime el primer y el último elemento de la lista.
+
+        
+    }
+}
