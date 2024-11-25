1c9020be6e26b24890d0a80a2e649f04ad5311f8
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-25 09:38:22 -0300

    Herencia

--- POO/src/PruebaVehiculos.java
+++ POO/src/PruebaVehiculos.java
@@ -2,10 +2,10 @@ public class PruebaVehiculos {
     public static void main(String[] args) {
 
 
-    Auto auto = new Auto("Chevrolet","Camaro",1960,4);
+    Auto auto = new Auto("Toyota", "Corolla", 2023, 4);
     auto.mostrarDetalles();
 
-    Motocicleta motocicleta = new Motocicleta("Indian","Scout",1980,"Seguro");
+    Motocicleta motocicleta = new Motocicleta("Honda", "CBR600RR", 2022, "Integral");
     motocicleta.mostrarDetalles();
     }
 }
