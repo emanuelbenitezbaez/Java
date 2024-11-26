89d56f2aa751ba9690b928dbb39cf334972f4ed2
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-26 09:27:01 -0300

    Herencia

--- POO/src/Colaborador.java
+++ POO/src/Colaborador.java
@@ -1,7 +1,29 @@
 public class Colaborador extends  Usuario{
 
 
-    public Colaborador(int id, String dni, String nombre, String apellido, String domicilio, String telefono) {
+    int num_legajo;
+    String cargo;
+    double sueldo;
+
+
+    public Colaborador(int id, String dni, String nombre, String apellido, String domicilio, String telefono, int num_legajo, String cargo, double sueldo) {
         super(id, dni, nombre, apellido, domicilio, telefono);
+        this.num_legajo = num_legajo;
+        this.cargo = cargo;
+        this.sueldo = sueldo;
+    }
+
+    public void mostrarDetalles () {
+        System.out.println("Informacion del empleado:");
+        System.out.println("Id: " +id);
+        System.out.println("DNI: "+dni);
+        System.out.println("Nombre:"+nombre);
+        System.out.println("Apellido:"+apellido);
+        System.out.println("Domicilio:"+domicilio);
+        System.out.println("Telefono:"+telefono);
+        System.out.println("Numero de legajo:"+num_legajo);
+        System.out.println("Cargo:"+cargo);
+        System.out.println("Sueldo:"+sueldo);
+
     }
 }
---
+++ POO/src/EjecutarHerencia.java
@@ -0,0 +1,6 @@
+public class EjecutarHerencia {
+    public static void main(String[] args) {
+        Colaborador colaborador = new Colaborador(04,"93884916","Emanuel","Benitez Baez","Chimondegui 1024","1198766554",897,"Estudiante",1887009);
+        colaborador.mostrarDetalles();
+    }
+}
