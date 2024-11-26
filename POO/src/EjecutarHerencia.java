8c1cd27caa1c58347d4c1b68fe4f5f9d1c72b765
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-26 09:36:26 -0300

    Herencia

--- POO/src/EjecutarHerencia.java
+++ POO/src/EjecutarHerencia.java
@@ -2,5 +2,9 @@ public class EjecutarHerencia {
     public static void main(String[] args) {
         Colaborador colaborador = new Colaborador(04,"93884916","Emanuel","Benitez Baez","Chimondegui 1024","1198766554",897,"Estudiante",1887009);
         colaborador.mostrarDetalles();
+
+        Consultor consultor = new Consultor(04,"93884916","Emanuel","Benitez Baez","Chimondegui 1024","1198766554");
+        //consultor.getNombre();
+        System.out.println("Nombre del consultor:" +consultor.getNombre());
     }
 }
