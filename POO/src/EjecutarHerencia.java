a64af065ab64a0b83f5f7e03faa3705e7c3473ab
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-26 09:52:46 -0300

    practica herencia

--- POO/src/EjecutarHerencia.java
+++ POO/src/EjecutarHerencia.java
@@ -4,11 +4,18 @@ public class EjecutarHerencia {
         colaborador.mostrarDetalles();
 
         Consultor consultor = new Consultor(4,"93884916","Emanuel","Benitez Baez","Chimondegui 1024","1198766554","Globant",23);
-
-
         consultor.mostrarDetallesDeConsultor();
 
 
+        //Colaborador:
+        System.out.println("Colaborador");
+        System.out.println("Domicilio del colaborador: " + colaborador.getDomicilio());
+        System.out.println("Numero de legajo del colaborador: " + colaborador.getNum_legajo());
 
+        //Consultor:
+        System.out.println("Consultor");
+        System.out.println("Domicilio del consultor: " + consultor.getDomicilio());
+        System.out.println("Numero de legajo del consultor: " + consultor.getNumero_consultor());
+        System.out.println("Consultora: " + consultor.getNombre_consultora());
     }
 }
