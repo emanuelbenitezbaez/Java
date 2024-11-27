82d89b37559b81ba8fc7e19ac4baa8ca409dc2ad
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-11-27 10:05:00 -0300

    Clases y metodos abastractos

--- Programacio Orientada a Objetos/src/Circulo.java
+++ Programacio Orientada a Objetos/src/Circulo.java
@@ -1,3 +1,12 @@
-public class Circulo {
+public class Circulo extends Figura {
 
+
+    public Circulo(double x, double y) {
+        super(x, y);
+    }
+
+    @Override
+    public double calcularArea() {
+        return 0;
+    }
 }
--- Programacio Orientada a Objetos/src/Cuadrado.java
+++ Programacio Orientada a Objetos/src/Cuadrado.java
@@ -1,3 +1,12 @@
-public class Cuadrado {
-    
+public class Cuadrado extends Figura {
+
+
+    public Cuadrado(double x, double y) {
+        super(x, y);
+    }
+
+    @Override
+    public double calcularArea() {
+        return 0;
+    }
 }
