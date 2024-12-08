33b93d77adbc702130e376d3e02047ada76bb599
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-08 09:56:29 -0300

    carga de arreglos y calculo de promedio de notas

--- Arrays/src/Promedio.java
+++ Arrays/src/Promedio.java
@@ -14,15 +14,13 @@ public class Promedio {
 
 
         //Cargamos notas
-
-        int contador=0;
+        
         int acumulador=0;
         int resultado;
         for (int i =0; i < entrada;i++){
             System.out.println("Ingrese la nota " + i + ": ");
             cantidad_notas[i] = consola.nextInt();
             acumulador =acumulador +  cantidad_notas[i];
-            contador++;
         }
 
         System.out.println("Cantidad de notas ingresadas: " + entrada);
@@ -32,7 +30,7 @@ public class Promedio {
             System.out.println("Nota "+ i + ": " + cantidad_notas[i]);
         }
 
-        resultado = acumulador / contador;
+        resultado = acumulador / entrada;
 
         System.out.println("Promedio: " + resultado);
 
