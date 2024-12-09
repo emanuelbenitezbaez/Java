b7be976b19886929d23d6ce422db0fa5bb19351d
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-09 17:33:00 -0300

    matrices

--- Arrays/src/Matriz.java
+++ Arrays/src/Matriz.java
@@ -1,26 +1,28 @@
+
 public class Matriz {
     public static void main(String[] args) {
         // Definir el tamaño de la matriz
-        int[][] matriz = new int[3][3];
-        // Asignar valores individualmente
-        matriz[0][0] = 1;
-        matriz[0][1] = 2;
-        matriz[0][2] = 3;
-        matriz[1][0] = 4;
-        matriz[1][1] = 5;
-        matriz[1][2] = 6;
-        matriz[2][0] = 7;
-        matriz[2][1] = 8;
-        matriz[2][2] = 9;
 
-        System.out.println("Matriz [0][0] : "+ matriz[0][0]);
-        System.out.println("Matriz [0][1] : "+ matriz[0][1]);
-        System.out.println("Matriz [0][2] : "+ matriz[0][2]);
-        System.out.println("Matriz [1][0] : "+ matriz[1][0]);
-        System.out.println("Matriz [1][1] : "+ matriz[1][1]);
-        System.out.println("Matriz [1][2] : "+ matriz[1][2]);
-        System.out.println("Matriz [2][0] : "+ matriz[2][0]);
-        System.out.println("Matriz [2][1] : "+ matriz[2][1]);
-        System.out.println("Matriz [2][2] : "+ matriz[2][2]);
+        final int FILAS =3;
+        final int COLUMNAS = 3;
+        int[][] matriz = new int[FILAS][COLUMNAS];
+        // Asignar valores individualmente
+        matriz[0][0] = 100;
+        matriz[0][1] = 200;
+        matriz[0][2] = 300;
+        matriz[1][0] = 400;
+        matriz[1][1] = 500;
+        matriz[1][2] = 600;
+        matriz[2][0] = 700;
+        matriz[2][1] = 800;
+        matriz[2][2] = 900;
+
+        for (int i=0;i < FILAS;i++){
+                for (int j=0;j < COLUMNAS;j++ ){
+                    System.out.println("FILA " + i +" COL "+ j +"--> VALOR: " +matriz[i][j]);
+                }
+        }
+
+
     }
 }
