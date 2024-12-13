1be716ff768bca6c1f2cedda9d05671e3848db40
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-13 14:33:09 -0300

    Metodos estaticos

--- AtributosMetodosEstaticos/src/metodosestaticos/Main.java
+++ AtributosMetodosEstaticos/src/metodosestaticos/Main.java
@@ -1,7 +1,28 @@
 package metodosestaticos;
 
+
 public class Main {
     public static void main(String[] args) {
+
+
+        Persona persona1 = new Persona("Emanuel","Benitez");
+        Persona persona2 = new Persona("Paula","Federico");
+        Persona persona3 = new Persona("Nombre","Apellido");
+
+        System.out.println(persona1);
+        System.out.println(persona2);
+        System.out.println(persona3);
+
+
+        System.out.println("\nCantidad total de objetos persona creados: " + Persona.getNumeroPersonas());
+
+
+
+        //El metodo estatico es igual a atributos estaticos pero accedemos a atributos privados estaticos atraves de metodos estaticos.
+
+        // Creamos el metodo que necesitemos eje: public static int getNumeroPersonas()
+
+        // Y accedemos al atributo desde el Main mediante la clase padre ej: Persona.getNumeroPersonas()
 
     }
 }
--- AtributosMetodosEstaticos/src/metodosestaticos/Persona.java
+++ AtributosMetodosEstaticos/src/metodosestaticos/Persona.java
@@ -1,5 +1,32 @@
 package metodosestaticos;
 
 public class Persona {
-    
+
+
+    //Estatico es cuando una variable que se asocia con una clase en si misma y no con sus objetos
+    private static int numeroPersonas= 0;
+    private String name;
+    private String username;
+    private int idPersonas ;
+
+
+    public Persona(String name, String username) {
+        this.name = name;
+        this.username = username;
+        Persona.numeroPersonas++;
+        this.idPersonas = Persona.numeroPersonas;
+
+    }
+
+    @Override
+    public String toString () {
+        return  "\nName: "+ this.name + "\nUsername: " + this.username + "\nId: " + idPersonas ;
+    }
+
+
+
+    public static int getNumeroPersonas() {
+        return Persona.numeroPersonas;
+    }
+
 }
