30edff17dd9f5b604a4e1bc672b0d279016470e1
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-05 17:18:27 -0300

    Ejercicio4

---
+++ Javas/src/Practica/Ejercicio4.java
@@ -0,0 +1,28 @@
+package Practica;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        /*
+        * Entrada: Una secuencia de números enteros positivos que termina en 0.
+
+Tarea: Obtener la suma de aquellos números cuya cantidad de cifras sea igual a la suma de esas mismas cifras.
+
+Restricción: No se pueden utilizar arrays.
+
+Ejemplo de entrada: 1, -111, 5, 111, 66, 201, 27, 0
+
+Salida esperada: 1 + 111 + 201 = 313
+        * */
+        int numeros;
+        int suma;
+
+
+        Scanner entrada = new Scanner(System.in);
+         numeros = entrada.nextInt();
+
+         
+
+    }
+}
