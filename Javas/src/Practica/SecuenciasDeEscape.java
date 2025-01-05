1327e851f1d6a9fe27b9b29a9c6c6ace29d66ca3
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-05 10:18:50 -0300

    secuencias de escape

---
+++ Javas/src/Practica/SecuenciasDeEscape.java
@@ -0,0 +1,15 @@
+package Practica;
+
+public class SecuenciasDeEscape {
+    //Secuencias de escape
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Hola \n mundo");//Salto de linea
+        System.out.println("Hola \t mundo");//Tabulador
+        System.out.println("Hola \b mundo");//Retroceso
+        System.out.println("Hola \r mundo");//Retorno de carro
+        System.out.println("Hola \f mundo");//Salto de pagina
+        System.out.println("Hola \\ mundo");//Barra invertida
+        System.out.println("Hola \" mundo");//Comillas dobles
+    }
+}
