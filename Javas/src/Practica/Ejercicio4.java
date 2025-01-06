cb7fcb573206d07c80d603435d61b484854038c0
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2025-01-06 10:37:14 -0300

    Ejercicio 4

--- Javas/src/Practica/Ejercicio4.java
+++ Javas/src/Practica/Ejercicio4.java
@@ -2,6 +2,8 @@ package Practica;
 
 import java.util.Scanner;
 
+
+
 public class Ejercicio4 {
     public static void main(String[] args) {
         /*
@@ -15,14 +17,37 @@ Ejemplo de entrada: 1, -111, 5, 111, 66, 201, 27, 0
 
 Salida esperada: 1 + 111 + 201 = 313
         * */
-        int numeros;
-        int suma;
 
 
         Scanner entrada = new Scanner(System.in);
-         numeros = entrada.nextInt();
+        String resultado = "";
+        int total = 0;
+        int numero ;
 
-         
+         do {
+
+
+             System.out.println("Ingrese numeros enteros y termine con 0:");
+             numero = entrada.nextInt();
+             int cifras= 1;
+             int suma = numero % 10;
+             int aux = numero;
+
+             while (aux>9){
+                 aux = aux / 10;
+                 suma += aux % 10;
+                 cifras++;
+             }
+             if (cifras == suma){
+                 resultado += numero + " + ";
+                 total += numero;
+             }
+
+         }while (numero != 0);
+        System.out.println("La suma de los numeros cuya cantidad de cifras sea igual a la suma de esas mismas cifras es: " + resultado + " = " + total);
+
+
+
 
     }
 }
---
+++ Javas/src/Practica/Ejercicio5.java
@@ -0,0 +1,7 @@
+package Practica;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        
+    }
+}
